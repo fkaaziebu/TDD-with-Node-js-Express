a5d8d7b954e2e4fd7c64d24c1273f2f59050affa
fkaaziebu <frederickaziebu1998@gmail.com>
2023-03-27 04:18:52 +0000

    Activation Email
    Sending account activation email with activation token

---
+++ src/config/emailTransporter.js
@@ -0,0 +1,6 @@
+const nodemailerStub = require("nodemailer-stub");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport(nodemailerStub.stubTransport);
+
+module.exports = transporter;
